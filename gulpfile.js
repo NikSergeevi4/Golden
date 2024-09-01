fa8b28f5427dce78c06128c325791eabf7ef7fe1
Oleksandr Malynin <podhvatovshurik@gmail.com>
2024-09-02 00:18:02 +0200

    fix: started refactoring gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -30,39 +30,39 @@ gulp.task("styles", function () {
 gulp.task("watch", function () {
 	gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"));
 	gulp.watch("src/*.html").on("change", gulp.parallel("html"));
-	gulp.watch("src/js/**/*.js").on("change", gulp.parallel("scripts"));
-	gulp.watch("src/fonts/**/*").on("all", gulp.parallel("fonts"));
-	gulp.watch("src/icons/**/*").on("all", gulp.parallel("icons"));
+	// gulp.watch("src/js/**/*.js").on("change", gulp.parallel("scripts"));
+	// gulp.watch("src/fonts/**/*").on("all", gulp.parallel("fonts"));
+	// gulp.watch("src/icons/**/*").on("all", gulp.parallel("icons"));
 	gulp.watch("src/img/**/*").on("all", gulp.parallel("images"));
 });
 
 gulp.task("html", function () {
 	return gulp
 		.src("src/*.html")
-		.pipe(htmlmin({ collapseWhitespace: true }))
+		// .pipe(htmlmin({ collapseWhitespace: true }))
 		.pipe(gulp.dest("dist/"));
 });
 
-gulp.task("scripts", function () {
-	return gulp
-		.src("src/js/**/*.js")
-		.pipe(gulp.dest("dist/js"))
-		.pipe(browserSync.stream());
-});
+// gulp.task("scripts", function () {
+// 	return gulp
+// 		.src("src/js/**/*.js")
+// 		.pipe(gulp.dest("dist/js"))
+// 		.pipe(browserSync.stream());
+// });
 
-gulp.task("fonts", function () {
-	return gulp
-		.src("src/fonts/**/*")
-		.pipe(gulp.dest("dist/fonts"))
-		.pipe(browserSync.stream());
-});
+// gulp.task("fonts", function () {
+// 	return gulp
+// 		.src("src/fonts/**/*")
+// 		.pipe(gulp.dest("dist/fonts"))
+// 		.pipe(browserSync.stream());
+// });
 
-gulp.task("icons", function () {
-	return gulp
-		.src("src/icons/**/*")
-		.pipe(gulp.dest("dist/icons"))
-		.pipe(browserSync.stream());
-});
+// gulp.task("icons", function () {
+// 	return gulp
+// 		.src("src/icons/**/*")
+// 		.pipe(gulp.dest("dist/icons"))
+// 		.pipe(browserSync.stream());
+// });
 
 gulp.task("images", function () {
 	return gulp
@@ -77,9 +77,9 @@ gulp.task(
 		"watch",
 		"server",
 		"styles",
-		"scripts",
-		"fonts",
-		"icons",
+		// "scripts",
+		// "fonts",
+		// "icons",
 		"html",
 		"images"
 	)
